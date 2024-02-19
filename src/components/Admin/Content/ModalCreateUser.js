120f5cd92e9b064cdcfb14054954fb9cfda58e48
nguye <nguyenphuyfgw@gmail.com>
2024-02-19 23:07:26 +0700

    validate data and add toastify

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,11 +3,21 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FaPlus } from "react-icons/fa";
+import { toast } from 'react-toastify';
+
 
 const ModalCreateUser = (props) => {
   const {show,setShow} = props;
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false)
+    setEmail("")
+    setPassword("")
+    setUsername("")
+    setRole("USER")
+    setImage("")
+    setPreviewImage("")
+  };
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,15 +35,19 @@ const ModalCreateUser = (props) => {
     }
   };
   const handleSubmitCreateUser = async() => {
-    // call api
-    // let data = {
-    //   email: email,
-    //   password: password,
-    //   username: username,
-    //   role: role,
-    //   userImage: image
-    // }
-    // console.log(data)
+    // validate
+    const isValidEmail = validateEmail(email)
+    if(!isValidEmail) {
+      toast.error("Invalid Email")
+      
+      return;
+    }
+    if(!password) {
+      toast.error("Invalid Password")
+      return
+    }
+
+    // submit data
     const data = new FormData();
     data.append('email', email);
     data.append('password', password);
@@ -42,8 +56,23 @@ const ModalCreateUser = (props) => {
     data.append('userImage', image);
 
     let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-    console.log("check res: ",res);
+    console.log("check res: ",res.data);
+    if(res.data && res.data.EC == 0) {
+      toast.success(res.data.EM)
+      handleClose()
+    }
+    if(res.data && res.data.EX !== 0) {
+      toast.error(res.data.EM)
+    }
   }
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   return (
     <>
