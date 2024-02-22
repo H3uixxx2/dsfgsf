3097fde55085bbd035691dfa7e6bc98f77817fa1
nguye <nguyenphuyfgw@gmail.com>
2024-02-23 01:32:21 +0700

    update list of users when adding successfully

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,13 +1,24 @@
 import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FaPlus } from "react-icons/fa";
-import { useState } from "react";
 import TableUser from "./TableUser";
-
+import { useEffect, useState } from "react"
+import {getAllUsers} from '../../../services/apiService'
 
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false)
+  const [listUsers,setListUsers] = useState([])
 
+    useEffect(()=>{
+      fetchListUsers();
+  },[])
+
+  const fetchListUsers = async() => {
+      let res =  await getAllUsers()
+      if(res.EC === 0) {
+          setListUsers(res.DT)
+      }
+  }
 
   return (
     <div className="manage-user-container">
@@ -18,11 +29,12 @@ const ManageUser = (props) => {
             <FaPlus/> Add new user</button>
         </div>
         <div className="table-users-container">
-          <TableUser/>
+          <TableUser listUsers={listUsers}/>
         </div>
         <ModalCreateUser 
           show={showModalCreateUser} 
           setShow={setShowModalCreateUser}
+          fetchListUsers={fetchListUsers}
         />
       </div>
     </div>
